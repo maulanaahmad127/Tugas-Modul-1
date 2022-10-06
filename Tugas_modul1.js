112c475763b55a51411cad55381b5d23bc4fafd1
Maulana Ahmad Rodhi Alfian <maulahmaddd40@gmail.com>
2022-10-06 20:13:01 +0700

    push object array

---
+++ Tugas_modul1.js
@@ -0,0 +1,177 @@
+const mahasiswa = [
+    {
+        nama_lengkap: "Fiki Rilo Pambudi",
+        nama_panggilan: "Fiki",
+        nomor_telpon: "089670641830",
+        id_line: "fiki_rilopambudi",
+        nim: 21120119120001,
+        email: "fikirilopambudi@gmail.com",
+        hobi: "menggambar"
+
+    },
+    {
+        nama_lengkap: "Nisrina Shofa Nadifa",
+        nama_panggilan: "Nisrina",
+        nomor_telepon: "081329459823",
+        id_line: "nsrndv",
+        tanggal_lahir: "06/10/2001",
+        nim: 21120119120002,
+        email: "nsrndv@gmail.com",
+        hobi: "Jalan-jalan"
+    },
+    {
+        nama_lengkap: "Nurhaidah",
+        nama_panggilan: "Ida",
+        nomor_telepon: "083839509288",
+        id_line: "haidah.",
+        nim: 21120119120003,
+        email: "nurhaidah586@gmail.com",
+        hobi: "membaca"
+    },
+    {
+        nama_lengkap: "Shafiyah Huyai",
+        nama_panggilan: "Aay",
+        nomor_telepon: "081932562688",
+        id_line: "Shafiyahhuyai11",
+        tanggal_lahir: "11/27/2000",
+        nim: 21120119120004,
+        email: "shafiyahhuyay@gmail.com",
+        hobi: "Memasak dan menyanyi"
+    },
+    {
+        nama_lengkap: "Taufiqurrohman",
+        nama_panggilan: "Taufiq",
+        nomor_telepon: "082328919930",
+        id_line: "ipik3",
+        tanggal_lahir: "10/20/2000",
+        nim: 21120119120005,
+        mail: "taufiqurrohman010@gmail.com",
+        hobi: "main game"
+    },
+    {
+        nama_lengkap: "Khairun Nisa Maulani",
+        nama_panggilan: "Nisa",
+        nomor_telepon: "082272506528",
+        id_line: "khairunnisamaulani05",
+        tanggal_lahir: "05/04/2001",
+        nim: 21120119120006,
+        email: "khairunnisamaulani27@gmail.com",
+        hobi: "Badminton"
+    },
+    {
+        nama_lengkap: "Cinka Sihaloho",
+        nama_panggilan: "Cinka",
+        nomor_telepon: "0895354209889",
+        id_line: "cinkaaashl",
+        nim: 21120119120007,
+        email: "cinkasihaloho91@gmail.com",
+        hobi: "main game"
+    },
+    {
+        nama_lengkap: "Galih Saputra",
+        nama_panggilan: "Gatra",
+        nomor_telepon: "085712247539",
+        id_line: "magatras13",
+        tanggal_lahir: "06/16/2000",
+        nim: 21120119120008,
+        email: "gatra881@gmail.com",
+        hobi: "memancing, membaca komik"
+    },
+    {
+        nama_lengkap: "Helmi Bagus Wijanarko",
+        nama_panggilan: "Helmi",
+        nomor_telepon: 895348805160,
+        id_line: "Helmibagus1",
+        tanggal_lahir: "09/02/2001",
+        nim: 21120119120009,
+        email: "Helmibagus8@gmail.com",
+        hobi: "Nonton film,renang"
+    },
+    {
+        nama_lengkap: "Samuel Imanta Ginting",
+        nama_panggilan: "Samuel",
+        nomor_telepon: "082210562818",
+        id_line: "samuelginting02",
+        nim: 21120119120010,
+        email: "samuelginting6264@gmail.com",
+        hobi: "berolah raga"
+    },
+    {
+        nama_lengkap: "Syahrul Ramadhan",
+        nama_panggilan: "Ramadhan",
+        nomor_telepon: "085376744296",
+        id_line: "syahrulrama17",
+        tanggal_lahir: "12/17/2000",
+        nim: 21120119120011,
+        mail: "syahrulrama017@gmail.com",
+        hobi: "Game,melukis,nonton"
+    },
+    {
+        ama_lengkap: "Anhar",
+        nama_panggilan: "Aan",
+        nomor_telepon: "085271572462",
+        id_line: "anhar.id",
+        tanggal_lahir: "06/25/2001",
+        nim: 21120119120012,
+        email: "cleverboyz.id@gmail.com",
+        hobi: "main game, nonton film"
+    },
+    {
+        nama_lengkap: "Bagus Muhamad Iqbal",
+        nama_panggilan: "Bagus",
+        nomor_telepon: "087889533529",
+        id_line: 292027012018,
+        nim: 21120119120013,
+        email: "iqballfriendly@gmail.com",
+        hobi: "GAMEONLINE"
+    },
+    {
+        nama_lengkap: "Erlin Sofia Sitohang",
+        nama_panggilan: "Erlin",
+        nomor_telepon: "085382537715",
+        id_line: "erlinsoviaa_sitohang",
+        nim: 21120119120014,
+        email: "erlin.vivoy21@gmail.com",
+        hobi: "berenang"
+    },
+    {
+        nama_lengkap: "Chairullatif Aji Sadewa",
+        nama_panggilan: "Khoirul",
+        nomor_telepon: "085741163476",
+        id_line: "khoirullatif337",
+        tanggal_lahir: "05/14/2001",
+        nim: 21120119120015,
+        email: "khoirullatif337@gmail.com",
+        hobi: "voli"
+    },
+    {
+        nama_lengkap: "Anggilia Nur Safitri",
+        nama_panggilan: "Anggi",
+        nomor_telepon: "082144698861",
+        id_line: "anggilianursafitri",
+        nim: 21120119120016,
+        email: "anggilianursafitri9@gmail.com"
+    },
+    {
+        nama_lengkap: "Hajime Yudha Iqbal Pranata",
+        nama_panggilan: "Hajime",
+        nomor_telepon: "089659178560",
+        id_line: "hajimeyudha1",
+        tanggal_lahir: "-",
+        nim: 21120119120017,
+        email: "scorpionid078@gmail.com",
+        hobi: "Komputer"
+    },
+    {
+        nama_lengkap: "Desy Lusiana Sitompul",
+        nama_panggilan: "Desy",
+        nomor_telepon: "085820788083",
+        id_line: "desy.ls",
+        tanggal_lahir: "12/15/2000",
+        nim: 21120119120018,
+        email: "desylusianasitompul649@gmail.com",
+        hobi: "dengar musik"
+    }
+
+
+];
